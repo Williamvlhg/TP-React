72647c6ce41dda207ae9145a0336d15b9fd7d004
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2024-01-31 15:53:16 +0100

    Update App.js

--- tp-react/src/App.js
+++ tp-react/src/App.js
@@ -9,20 +9,19 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <BrowserRouter>
+        <Routes>
+
+            <Route path="/" element={<Home />} />
+            <Route path="/avis" element={<Avis />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/mentions" element={<Mentions />} />
+
+        </Routes>
+        
+        
+        
+        </BrowserRouter>
     </div>
   );
 }
