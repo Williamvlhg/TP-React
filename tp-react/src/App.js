422537560afc46885d3ee2b3f4f38427016c8c5d
Williamvlhg <136070530+Williamvlhg@users.noreply.github.com>
2024-02-01 10:05:37 +0100

    zqsd
    zqsd

--- tp-react/src/App.js
+++ tp-react/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Home from './pages/Home';  
 import Avis from './pages/Avis';
 import Contact from './pages/Contact';
 import Mentions from './pages/Mentions';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
---
+++ tp-react/src/components/Header/index.js
@@ -0,0 +1,16 @@
+
+
+const Header = ({ children }) => {
+
+    return (
+        <header className="App-header">
+            <div>
+            {children}
+            </div>
+        </header>
+    )
+
+
+}
+
+export default Header;
